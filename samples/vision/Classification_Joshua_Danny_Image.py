a04bd61e1d3d1e49624991986ef9202543ba0b95
bjspeaks <bjspeaks@gmail.com>
2020-06-30 20:38:06 +0100

    added the prediction_vision logic and also child identification

--- samples/vision/Classification_Joshua_Danny_Image.py
+++ samples/vision/Classification_Joshua_Danny_Image.py
@@ -22,39 +22,39 @@ training_key = "1d09912649e14a70a777687237b4e39d"
 prediction_key = "8a6e3ffc93d84b6ab547d944612e8b9c"
 prediction_resource_id = "/subscriptions/ba66f898-5c3d-473b-953e-fefd816b4264/resourceGroups/packing/providers/Microsoft.CognitiveServices/accounts/packimagerecogniti-Prediction"
 
-publish_iteration_name = "classifyModel"
+publish_iteration_name = "classifyMyChildren"
 
 credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
 trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
 
 # Create a new project
 print ("Creating project...")
-project = trainer.create_project("ImageClassification")
+project = trainer.create_project("ClassifyMyChildren")
 
 # Make two tags in the new project
 
 
-hemlock_tag = trainer.create_tag(project.id, "Hemlock")
-cherry_tag = trainer.create_tag(project.id, "Japanese Cherry")
+Danny_tag  = trainer.create_tag(project.id, "Danny")
+Joshua_tag = trainer.create_tag(project.id, "Joshua")
 
 
-base_image_url = "C:\\Users\\blessonj\\Python\\cognitive-services-python-sdk-samples\\samples\\vision\\"
+base_image_url = "C:\\Users\\blessonj\\Python\\"
 
 print("Adding images...")
 
 image_list = []
 batching_list = []
 
-for image_num in range(1, 11):
-    file_name = "hemlock_{}.jpg".format(image_num)
-    with open(base_image_url + "images\\Hemlock\\" + file_name, "rb") as image_contents:
-        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[hemlock_tag.id]))
+for image_num in range(1,12):
+    file_name = "Danny_{}.jpg".format(image_num)
+    with open(base_image_url + "Danny\\" + file_name, "rb") as image_contents:
+        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[Danny_tag.id]))
         
 
-for image_num in range(1, 11):
-    file_name = "japanese_cherry_{}.jpg".format(image_num)
-    with open(base_image_url + "images\\Japanese Cherry\\" + file_name, "rb") as image_contents:
-        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[cherry_tag.id]))
+for image_num in range(1, 8):
+    file_name = "Joshua_{}.jpg".format(image_num)
+    with open(base_image_url + "Joshua\\" + file_name, "rb") as image_contents:
+        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[Joshua_tag.id]))
         '''batching_list.append=ImageFileCreateBatch(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[hemlock_tag.id]), tag_ids=[hemlock_tag.id])'''
 
 upload_result = trainer.create_images_from_files(project.id,images=image_list)
